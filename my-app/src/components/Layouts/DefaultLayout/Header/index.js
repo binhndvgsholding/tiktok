9bbd8c8706b0a53300a857c6c40c2864b28b2d5e
binhndvgsholding <115780370+binhndvgsholding@users.noreply.github.com>
2023-01-03 17:36:46 +0700

    update code

--- my-app/src/components/Layouts/DefaultLayout/Header/index.js
+++ my-app/src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,5 +1,61 @@
+import { Link } from 'react-router-dom'
+import { 
+    FaSearch, 
+    FaPlus, 
+    FaPaperPlane,
+    FaRegCommentAlt
+} from "react-icons/fa";
+import './Header.scss'
 function Header(){
-    return <h2>Header</h2>
+    return (
+        <header  className="header-top" >
+            <div className='tiktok-logo'>
+                <Link to="/">
+                    <img className='logo' alt='logo'src='/img/1200px-TikTok_logo.svg.png' />
+                </Link>
+            </div>
+            <div className='tiktok-thiumz-DivSearchFormContainer '>
+                <div className='tiktok-1h3oqen-DivHeaderCenterContainer'>
+                    <form className='fromSearch'>
+                                <input
+                                    type="search"
+                                    className='searchInput' 
+                                    placeholder="Search accounts and videos"
+                                    autocomplete="off"
+                                />
+                                <span className='tiktok-hck1rr-SpanSpliter'></span>
+                                <button className='search-button' type='submit'>
+                                    <FaSearch className='icon-search' />
+                                </button>
+                    </form> 
+                </div>
+                   
+            </div>
+            <div  className='tiktok-ba55d9-DivHeaderRightContainer'>
+                <div className='upload-icon'>
+                    <Link  to='/upload'>
+                        <div className='tiktok-1qup28j-DivUpload' >
+                            <FaPlus />
+                            <span className='tiktok-y3rt08-SpanUploadText'>Upload</span>
+                        </div>
+                    </Link>
+                </div>
+                <div className='top-dm-icon'>
+                    <Link  to='/upload'>
+                        <span className='tiktok-9oofjg-StyledIcon'><FaPaperPlane/></span>
+                    </Link>
+                </div>
+                <div className='inbox-icon'>
+                    <Link  to='/upload'>
+                        <span className='tiktok-9oofjg-StyledIcon'><FaRegCommentAlt/></span>
+                    </Link>
+                </div>
+                <div className='profile-icon'>
+                    
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
--- my-app/src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ my-app/src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,126 @@
+import { Link } from 'react-router-dom'
+import { 
+    FaHome,
+    FaUserFriends,
+    FaVideo,
+    FaRegCheckCircle,
+    FaRegPlayCircle
+} from "react-icons/fa";
+import './Sidebar.scss'
 function Sidebar(){
-    return <h2>Sidebar</h2>
+    return (
+        <nav className='tiktok-r0hg2a-DivSideNavContainer'>
+           <div className='tiktok-1a4urrd-DivWrapper '>
+                <div className='tiktok-DivScrollContainer-StyledScroll'>
+                    <div className='tiktok-1inll25-DivMainNavContainer'>
+                        <div>
+                            <Link to='/upload' className='tiktok-1s4a8it-StyledTmpLink'>
+                                <FaHome className='icon-sidebar' />
+                                <h2 className='tiktok-lhjwdg-H2MainNavText'>For You</h2>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to='/upload' className='tiktok-1s4a8it-StyledTmpLink'>
+                                <FaUserFriends className='icon-sidebar' />
+                                <h2 className='tiktok-lhjwdg-H2MainNavText'>Following</h2>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to='/upload' className='tiktok-1s4a8it-StyledTmpLink'>
+                                <FaVideo className='icon-sidebar' />
+                                <h2 className='tiktok-lhjwdg-H2MainNavText'>LIVE</h2>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className ='tiktok-he1edq-DivUserContainer'>
+                        <p className='suggest-accounts'>Suggested accounts</p>
+                        <div className= 'tiktok-1mo2fkg-DivUserLinkContainer'>
+                            <Link to="/upload" className='suggest-user-avatar'>
+                                <div className='tiktok-n1lhfn-DivContainer-StyledUserAvatar'>
+                                    <span className='tiktok-13abja1-SpanAvatarContainer-StyledAvatar'>
+                                        <img src='https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1672909200&x-signature=xTQCpKUwCcND7VPL90RPSOinOKI%3D' className='tiktok-1zpj2q-ImgAvatar e1e9er4e1' />
+                                    </span>
+                                </div>
+                            </Link>
+                            <Link to="/upload" className='tiktok-1g0yu4c-StyledUserContentLink'>
+                                <div className='tiktok-yt7cul-DivUserTitleWrapper' >
+                                    <h4 className='suggest-user-title'>theanh28entertainment</h4>
+                                    <div className='suggest-user-bluev'>
+                                        <FaRegCheckCircle className='tiktok-shsbhf-StyledVerifyBadge'/>
+                                    </div>
+                                </div>
+                                <p className='suggest-user-desc'>Theanh28</p>
+                            </Link>
+                        </div>
+                        <div className= 'tiktok-1mo2fkg-DivUserLinkContainer'>
+                            <Link to="/upload" className='suggest-user-avatar'>
+                                <div className='tiktok-n1lhfn-DivContainer-StyledUserAvatar'>
+                                    <span className='tiktok-13abja1-SpanAvatarContainer-StyledAvatar'>
+                                        <img src='https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1672909200&x-signature=xTQCpKUwCcND7VPL90RPSOinOKI%3D' className='tiktok-1zpj2q-ImgAvatar e1e9er4e1' />
+                                    </span>
+                                </div>
+                            </Link>
+                            <Link to="/upload" className='tiktok-1g0yu4c-StyledUserContentLink'>
+                                <div className='tiktok-yt7cul-DivUserTitleWrapper' >
+                                    <h4 className='suggest-user-title'>theanh28entertainment</h4>
+                                    <div className='suggest-user-bluev'>
+                                        <FaRegCheckCircle className='tiktok-shsbhf-StyledVerifyBadge'/>
+                                    </div>
+                                </div>
+                                <p className='suggest-user-desc'>Theanh28 </p>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='tiktok-he1edq-DivUserContainer'>
+                        <p className='following-accounts'>
+                            Following accounts
+                        </p>
+                        <p className='tiktok-1qkr43s-PFollowEmptyHint'>
+                            Accounts you follow will appear here 
+                        </p>
+                    </div>
+                    <div className='tiktok-jvyzjv-DivDiscoverContainer'>
+                        <p className='nav-discover-title'>
+                            Discover
+                        </p>
+                        <div className='nav-discover-list'>
+                            <Link to='/upload' className='nav-discover-href'>
+                                <div className='tiktok-1avbtpl-DivDiscoverItemContainer'>
+                                   <FaRegPlayCircle className='tiktok-1hy6k9f-StyledNumber' /> 
+                                   <p className='tiktok-e841qv-PText'>suthatla</p>
+                                </div>
+                            </Link>
+                            <Link to='/upload' className='nav-discover-href'>
+                                <div className='tiktok-1avbtpl-DivDiscoverItemContainer'>
+                                   <FaRegPlayCircle className='tiktok-1hy6k9f-StyledNumber' /> 
+                                   <p className='tiktok-e841qv-PText'>suthatla</p>
+                                </div>
+                            </Link>
+                            <Link to='/upload' className='nav-discover-href'>
+                                <div className='tiktok-1avbtpl-DivDiscoverItemContainer'>
+                                   <FaRegPlayCircle className='tiktok-1hy6k9f-StyledNumber' /> 
+                                   <p className='tiktok-e841qv-PText'>suthatla</p>
+                                </div>
+                            </Link>
+                            <Link to='/upload' className='nav-discover-href'>
+                                <div className='tiktok-1avbtpl-DivDiscoverItemContainer'>
+                                   <FaRegPlayCircle className='tiktok-1hy6k9f-StyledNumber' /> 
+                                   <p className='tiktok-e841qv-PText'>suthatla</p>
+                                </div>
+                            </Link>
+                            <Link to='/upload' className='nav-discover-href'>
+                                <div className='tiktok-1avbtpl-DivDiscoverItemContainer'>
+                                   <FaRegPlayCircle className='tiktok-1hy6k9f-StyledNumber' /> 
+                                   <p className='tiktok-e841qv-PText'>suthatla</p>
+                                </div>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </nav>
+    )
 }
 
 export default Sidebar
